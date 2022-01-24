274f0a410c081b9ea0e1f73000104a93ca946571
CaffieneDuck <samrid.pandit@gmail.com>
2022-01-24 16:28:00 +0545

    CHORE: Comment seed.ts as it was causing unnecessary errors

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,32 +1,33 @@
-import { PrismaClient, User } from '@prisma/client';
+// import { PrismaClient, User } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-const users: User[] = [
-  {
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    email: 'samrid.pandit@gmail.com',
-    username: 'samrid',
-    id: 1,
-    name: 'samrid pandit',
-    password: 'noeu',
-    role: 'ADMIN',
-  },
-];
+// const users: User[] = [
+//   {
+//     createdAt: new Date(),
+//     updatedAt: new Date(),
+//     email: 'samrid.pandit@gmail.com',
+//     username: 'samrid',
+//     id: 1,
+//     name: 'samrid pandit',
+//     password: 'noeu',
+//     role: 'ADMIN',
+    
+//   },
+// ];
 
-const addUsers = async () => {
-  users.forEach(async (user) => {
-    try {
-      await prisma.user.create({
-        data: user,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  });
-};
+// const addUsers = async () => {
+//   users.forEach(async (user) => {
+//     try {
+//       await prisma.user.create({
+//         data: user,
+//       });
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   });
+// };
 
-addUsers()
-  .catch((e) => console.log(e))
-  .finally(() => prisma.$disconnect());
+// addUsers()
+//   .catch((e) => console.log(e))
+//   .finally(() => prisma.$disconnect());
