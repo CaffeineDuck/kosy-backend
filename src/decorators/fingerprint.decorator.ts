193ebd4b1d86edde16cc42a051c0827e1593bb37
CaffieneDuck <samrid.pandit@gmail.com>
2022-01-24 16:27:05 +0545

    FEAT: Adds constrain of being 32 char for fingerPrint

--- src/decorators/fingerprint.decorator.ts
+++ src/decorators/fingerprint.decorator.ts
@@ -4,7 +4,12 @@ import { Request } from 'express';
 export const FingerPrint = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request: Request = ctx.switchToHttp().getRequest();
+
     const fingerPrint = request.headers?.fingerprint;
+
+    if (!fingerPrint || fingerPrint.length != 32) {
+      return
+    }
     return fingerPrint;
   },
 );
